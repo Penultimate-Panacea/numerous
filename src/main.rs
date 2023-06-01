2c14f8120ad92dd50a32835baa9f8934906f4651
Penultimate-Panacea <rian+github@fantozzi.org>
2023-06-01 13:18:58 -0400

    it does not

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,8 @@ mod factorization_based;
 
 
 fn main() {
-    let mut j = 1;
     for i in factorization_based::TEST_POWERFUL{
         let a = factorization_based::is_powerful(i);
         println!("{a}");
     };
-    while j < 1000 {
-        let b = factorization_based::is_powerful(j);
-        println!("Is {j} powerful?:  {b}");
-        j += 1;
-    }
 }
