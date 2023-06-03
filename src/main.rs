cb2b9af1fafbd682844ef4b259d2ccd9cde7b110
Penultimate-Panacea <rian+github@fantozzi.org>
2023-06-03 18:45:02 -0400

    added debug testing for candidate func

--- src/main.rs
+++ src/main.rs
@@ -11,4 +11,12 @@ fn main() {
     factorization_based::TEST_POWERFUL.into_par_iter().for_each(|i| {
         println!("Is {i} powerful?: {}", factorization_based::is_powerful(i));
     });
+    println!("");
+    println!("");
+    println!("CANDIDATE FUNCTION");
+    println!("");
+    println!("");
+    factorization_based::TEST_POWERFUL.into_par_iter().for_each(|i| {
+        println!("Is {i} powerful?: {}", factorization_based::is_powerful_candiate_function(i));
+    });
 }
