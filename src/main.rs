592a7dd8af4035fa7eaba5b38e45ce8d62eaeb87
Penultimate-Panacea <rian+github@fantozzi.org>
2023-06-03 12:42:46 -0400

    Added rayon based loop

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@
 #![deny(clippy::pedantic)]
 #![warn(clippy::cargo)]
 
+use rayon::prelude::*;
+
 mod factorization_based;
 
 
 fn main() {
-    println!("{}", factorization_based::is_powerful(1)); // First Powerful
-    println!("{}", factorization_based::is_powerful(4)); // Second Powerful 2^2
-    println!("{}", factorization_based::is_powerful(8)); // Third Powerful 2^3
-    println!("{}", factorization_based::is_powerful(343)); // 7^3
-    println!("{}", factorization_based::is_powerful(441)); // 7^2*3^2
+    factorization_based::TEST_POWERFUL.into_par_iter().for_each(|i| {
+        println!("Is {i} powerful?: {}", factorization_based::is_powerful(i));
+    });
 }
